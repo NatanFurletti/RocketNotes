731145086b7c20d26f625ec1e043a837812c81d2
Natan Furletti <natan.furletti@outlook.com>
2024-01-26 11:38:53 -0300

    component header

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,12 @@
 import { Container } from "./styles";
+import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
 
 export function Details() {
   return (
     <Container>
-      <h1>Hello World!</h1>;
-      <Button title="caguei pro eslint" loading />
+      <Header />
+      <Button title="Voltar" />
     </Container>
   );
 }
