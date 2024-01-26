e7b6c81507c84eade63a264ef239e4ba34c046d4
Natan Furletti <natan.furletti@outlook.com>
2024-01-26 11:51:03 -0300

    foto usuário

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,16 @@
-import { Container } from "./styles";
+import { Container, Profile } from "./styles";
 
 export function Header() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Profile>
+        <img src="https://github.com/natanfurletti.png" alt="Foto do usuário" />
+
+        <div>
+          <spam>Bem-vindo</spam>
+          <strong>Natan Furletti</strong>
+        </div>
+      </Profile>
+    </Container>
+  );
 }
