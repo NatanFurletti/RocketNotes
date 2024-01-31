8f2ec051e7397d2e60b2d754c02c9ebb726931b7
Natan Furletti <natan.furletti@outlook.com>
2024-01-31 17:08:51 -0300

    feat: SignIn struture

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Home } from "./pages/Home";
 import { ThemeProvider } from "styled-components";
-import theme from "./styles/theme";
 import GlobalStyles from "./styles/global";
+
+import theme from "./styles/theme";
+
+import { SignIn } from "./pages/SignIn";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>
 );
