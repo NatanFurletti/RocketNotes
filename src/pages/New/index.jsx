74258c226420884bcc4c8435788f04bbac96e028
Natan Furletti <natan.furletti@outlook.com>
2024-01-31 19:09:38 -0300

    feat: add New / Textarea

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,24 @@
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { Container, Form } from "./styles";
+import { Textarea } from "../../components/Textarea";
+
+export function New() {
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">voltar</a>
+          </header>
+
+          <Input placeholder="Título" />
+          <Textarea placeholder="Observações" />
+        </Form>
+      </main>
+    </Container>
+  );
+}
