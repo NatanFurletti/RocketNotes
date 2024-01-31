1f2faaedc06886e09f4bde1a450ae8ecb5835d70
Natan Furletti <natan.furletti@outlook.com>
2024-01-31 17:50:39 -0300

    feat: add SignUp page

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Home } from "./pages/Home";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/global";
 
 import theme from "./styles/theme";
 
-import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignIn />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>
 );
